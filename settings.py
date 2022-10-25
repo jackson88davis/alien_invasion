57cc4a4c0016762abf51a3fc2294e926a7eca4a9
jackson88davis <jackson880davis@gmail.com>
2022-10-25 09:55:06 -0400

    shooting bullet and alien
    alien not loading

--- settings.py
+++ settings.py
@@ -6,12 +6,17 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 600
-        self.bg_color = (96, 96, 96)
+        self.bg_color = (230, 230, 230)
         # Ship settings
         self.ship_speed = 1.5
         # Bullet settings
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (64, 64, 64)
-        self.bullets_allowed = 3
+        self.bullet_color = (194, 23, 232)
+        self.bullets_allowed = 5
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet = 1
