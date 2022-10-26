cf18db85c706d3448b2bcd41dc8cd4ce4386506a
jackson88davis <jackson880davis@gmail.com>
2022-10-26 12:21:10 -0400

    Bullet, Aliens, and Losing
    Fixed the bullet not showing up, the aliens would not show up, but fixed that to, added the bullet/alien collision system, and added restarted the game when the alien reaches the end.

--- settings.py
+++ settings.py
@@ -6,17 +6,18 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
         # Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
         # Bullet settings
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_speed = 1.5
+        self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = (194, 23, 232)
-        self.bullets_allowed = 5
+        self.bullets_allowed = 2
         # Alien settings
-        self.alien_speed = 1.0
+        self.alien_speed = 2
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left.
-        self.fleet = 1
+        self.fleet_direction = 1
